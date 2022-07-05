a7b41ce5ac33ebb58960bf12299500219c533296
ssForz <druholskiy@gmail.com>
2022-07-05 02:11:18 +0200

    Bored

--- meet/meet/settings.py
+++ meet/meet/settings.py
@@ -75,8 +75,8 @@ WSGI_APPLICATION = 'meet.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.path.join(BASE_DIR, 'db_for_meet'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
