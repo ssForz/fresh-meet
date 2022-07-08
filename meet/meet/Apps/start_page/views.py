57b0e4a1f83a9f5dad09a55580fe1c1de5a0a26a
ssForz <druholskiy@gmail.com>
2022-07-09 00:04:26 +0200

    Created forms.py

--- meet/meet/Apps/start_page/views.py
+++ meet/meet/Apps/start_page/views.py
@@ -1,31 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseNotFound
-from django import forms
 
-
-class NameForm(forms.Form):
-    your_name = forms.CharField(label='your_name', max_length=20)
-
-class AgeForm(forms.Form):
-    your_age = forms.CharField(label='your_age', max_length = 2)
-
-class SexForm(forms.Form):
-    your_sex = forms.CharField(label='your_sex', max_length = 7)
-
-class TownForm(forms.Form):
-    your_town = forms.CharField(label='your_town', max_length = 30)
-
-class HobbyForm(forms.Form):
-    your_hobby = forms.CharField(label='your_hobby', max_length = 35)
-
-class MailForm(forms.Form):
-    your_mail = forms.CharField(label='your_mail', max_length = 30)
-
-class PasswordForm(forms.Form):
-    your_password = forms.CharField(label='your_password', max_length = 15)
-
-class PasswordCheckForm(forms.Form):
-    your_password2 = forms.CharField(label='your_password2', max_length = 15)
+from user.forms import NameForm, AgeForm, SexForm, TownForm, HobbyForm, MailForm, PasswordForm, PasswordCheckForm
 
 def index(request):
     return render(request, 'start_page/index.html') 
