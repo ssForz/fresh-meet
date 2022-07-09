d1cfcb24da42d3a1393d1043dadf67ff2c9e591c
ssForz <druholskiy@gmail.com>
2022-07-09 20:56:02 +0200

    Created Log_Person

--- meet/meet/settings.py
+++ meet/meet/settings.py
@@ -85,8 +85,17 @@ WSGI_APPLICATION = 'meet.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'post_db',
+
+        'USER': 'team2',
+
+        'PASSWORD': '1234',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
     }
 }
 
