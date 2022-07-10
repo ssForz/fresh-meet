f5e9f0e9b0357029e0355c333b9e1c8fd7f095e3
ssForz <druholskiy@gmail.com>
2022-07-10 20:07:57 +0200

    Added profile function

--- meet/meet/Apps/office/views.py
+++ meet/meet/Apps/office/views.py
@@ -1,4 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
+
+from django.template.loader import get_template
+
+from user.models import Person
+
+from django.http import HttpResponse
 
 def index(request):
-	return render(request, 'office/index.html')
+
+	check = request.COOKIES.get('csrftoken')
+
+	check_token = 0;
+
+	news = Person.objects.all()
+
+	for i in news:
+		if check == i.token:
+
+			check_token = 1
+
+
+			your_name = i.username
+
+			your_age = i.age
+			your_sex = i.sex                
+			your_town = i.town
+			your_hobby = i.interest
+			context2 = {
+				'your_name': your_name,
+				'your_age': your_age,
+				'your_sex': your_sex,
+				'your_town': your_town,
+				'your_hobby': your_hobby
+			}
+			return render(request, 'office/index.html', context = context2)
+
+
+	return render(request, 'entry/index.html')
