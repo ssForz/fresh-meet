3430b2c1fc62f81687e5a476864bfbc3f157d968
ssForz <druholskiy@gmail.com>
2022-07-11 02:50:11 +0200

    fixed views

--- meet/meet/Apps/start_page/views.py
+++ meet/meet/Apps/start_page/views.py
@@ -16,7 +16,7 @@ def reg_success(request):
         form_mail = MailForm(request.POST)
         form_password = PasswordForm(request.POST)
         form_password2 = PasswordCheckForm(request.POST)
-
+        form_telegram = TelegramForm(request.POST)
         if form_name.is_valid() and form_age.is_valid() and form_sex.is_valid() and form_town.is_valid() and form_hobby.is_valid() and form_mail.is_valid() and form_password.is_valid() and form_password2.is_valid():
             from user.models import Person
 
@@ -35,9 +35,11 @@ def reg_success(request):
 
             your_mail = form_mail.cleaned_data.get("your_mail")
 
+            your_telegram = form_telegram.cleaned_data.get("your_telegram")
+
             news = Person.objects.all()
             for i in news:
-                if i.email == your_mail:
+                if i.email == your_mail or i.telegram == your_telegram:
                     return render(request, 'registration/index.html')
             your_password = form_password.cleaned_data.get("your_password")
 
@@ -46,11 +48,12 @@ def reg_success(request):
             if your_password != your_password2: 
                 return render(request, 'registration/index.html')
 
-            b = Person(username = your_name, sex=your_sex, age = your_age, town = your_town, interest = your_hobby, rating = 0, email = your_mail, password = your_password, online = False, token = '')
+            b = Person(username = your_name, sex=your_sex, age = your_age, town = your_town, interest = your_hobby, rating = 0, email = your_mail, telegram = your_telegram, password = your_password, online = False, token = '')
             b.save()
             return render(request, 'start_page/index.html')
 
     return HttpResponseNotFound("Error")
+
 
 def log_success(request):
     if request.method == 'POST':
